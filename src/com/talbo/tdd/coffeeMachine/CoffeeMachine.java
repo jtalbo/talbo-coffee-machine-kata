128f55146e768542706d2c5c5bdd80f439fdc76d
jtalbo <44943150+jtalbo@users.noreply.github.com>
2018-11-11 15:31:37 +0100

    Add files via upload
    Third Iteration

--- src/com/talbo/tdd/coffeeMachine/CoffeeMachine.java
+++ src/com/talbo/tdd/coffeeMachine/CoffeeMachine.java
@@ -7,33 +7,86 @@ public class CoffeeMachine {
 		
 		String[] splittedCommand = command.split(":");
 		
-		verifyDrinkType(splittedCommand, order);
+		verifyOrderPayment(splittedCommand, order);
 		
-		System.out.println(order.toString());
 		
 		return order;
 	}
 
 	
-	private static Order verifyDrinkType(String[] splittedCommand, Order order) {
-		if ("T".equals(splittedCommand[0])) {
-			order.setDrink(Order.DrinkType.TEA);
-		} else if ("H".equals(splittedCommand[0])) {
-			order.setDrink(Order.DrinkType.CHOCOLATE);
-		} else if ("C".equals(splittedCommand[0])) {
-			order.setDrink(Order.DrinkType.COFFEE);
+	private static Order verifyOrderPayment(String[] splittedCommand, Order order) {
+		boolean enoughMoney = false;
+		Double expectedPriceForThisDrink;
+		String insertedCoin = splittedCommand[3].toString();
+		Double insertedCoinInDoubleValue = Double.parseDouble(insertedCoin);
+		
+		if ("T".equals(splittedCommand[0]) || "Th".equals(splittedCommand[0])) {
+			expectedPriceForThisDrink = 0.40;
+			System.out.println("You have select a Tea ! Please insert 0.40 cts ...");
+			if (insertedCoinInDoubleValue >= expectedPriceForThisDrink) {
+				enoughMoney = true;
+				order.setPrice(expectedPriceForThisDrink);
+			} else {
+				Double calculChange = expectedPriceForThisDrink - insertedCoinInDoubleValue;
+				String insertedCoinInDoubleFormat = String.format("%.2f", insertedCoinInDoubleValue);
+				String restMoney = String.format("%.2f", calculChange);
+				System.out.println("You have insert "+ insertedCoinInDoubleFormat + " cts, you need to insert "+ restMoney + " cts more.");
+			}
+		} else if ("H".equals(splittedCommand[0]) || "Hh".equals(splittedCommand[0])) {
+			expectedPriceForThisDrink = 0.50;
+			System.out.println("You have select a Chocolate ! Please insert 0.50 cts ...");
+			if (insertedCoinInDoubleValue >= expectedPriceForThisDrink) {
+				enoughMoney = true;
+				order.setPrice(expectedPriceForThisDrink);
+			} else {
+				Double calculChange = expectedPriceForThisDrink - insertedCoinInDoubleValue;
+				String insertedCoinInDoubleFormat = String.format("%.2f", insertedCoinInDoubleValue);
+				String restMoney = String.format("%.2f", calculChange);
+				System.out.println("You have insert "+ insertedCoinInDoubleFormat + " cts, you need to insert "+ restMoney + " cts more.");
+			}
+		} else if ("C".equals(splittedCommand[0]) || "Ch".equals(splittedCommand[0])) {
+			expectedPriceForThisDrink = 0.60;
+			System.out.println("You have select a Coffee ! Please insert 0.60 cts ...");
+			if (insertedCoinInDoubleValue >= expectedPriceForThisDrink) {
+				enoughMoney = true;
+				order.setPrice(expectedPriceForThisDrink);
+			} else {
+				Double calculChange = expectedPriceForThisDrink - insertedCoinInDoubleValue;
+				String insertedCoinInDoubleFormat = String.format("%.2f", insertedCoinInDoubleValue);
+				String restMoney = String.format("%.2f", calculChange);
+				System.out.println("You have insert "+ insertedCoinInDoubleFormat + " cts, you need to insert "+ restMoney + " cts more.");
+			}
+		} else if ("O".equals(splittedCommand[0])) {
+			expectedPriceForThisDrink = 0.60;
+			System.out.println("You have select a Orange Juice ! Please insert 0.60 cts ...");
+			if (insertedCoinInDoubleValue >= expectedPriceForThisDrink) {
+				enoughMoney = true;
+				order.setPrice(expectedPriceForThisDrink);
+			} else {
+				Double calculChange = expectedPriceForThisDrink - insertedCoinInDoubleValue;
+				String insertedCoinInDoubleFormat = String.format("%.2f", insertedCoinInDoubleValue);
+				String restMoney = String.format("%.2f", calculChange);
+				System.out.println("You have insert "+ insertedCoinInDoubleFormat + " cts, you need to insert "+ restMoney + " cts more.");
+			}
 		}
 		
-		prepareOrder(splittedCommand, order);
+		if (enoughMoney) {
+			prepareOrder(splittedCommand, order);
+		}
+		
 		
 		return order;
 	}
 
 	
 	private static Order prepareOrder(String[] splittedCommand, Order order) {
-		Integer drinkWithoutSugarAndStick = 1;
+		String noValue = "";
 		
-		if (drinkWithoutSugarAndStick.equals(splittedCommand.length)) {
+		extraHotDrink(splittedCommand, order);
+		
+		selectDrinkType(splittedCommand, order);
+		
+		if (noValue.equals(splittedCommand[1]) && noValue.equals(splittedCommand[2])) {
 			order.setNbSugar(0);
 			order.setHasStick(false);
 			order.setStickString("no");
@@ -54,9 +107,36 @@ public class CoffeeMachine {
 			}
 		
 		}
+
+		System.out.println(order.toString());
 		
 		return order;
 		
+	}
+	
+	private static void extraHotDrink(String[] splittedCommand, Order order) {
+		if ("Th".equals(splittedCommand[0])) {
+			splittedCommand[0] = splittedCommand[0].replace("Th", "T");
+			order.setExtraHot(true);
+		} else if ("Hh".equals(splittedCommand[0])) {
+			splittedCommand[0] = splittedCommand[0].replace("Hh", "H");
+			order.setExtraHot(true);
+		} else if ("Ch".equals(splittedCommand[0])) {
+			splittedCommand[0] = splittedCommand[0].replace("Ch", "C");
+			order.setExtraHot(true);
+		}
+	}
+	
+	private static void selectDrinkType(String[] splittedCommand, Order order) {
+		if ("T".equals(splittedCommand[0])) {
+			order.setDrink(Order.DrinkType.TEA);
+		} else if ("H".equals(splittedCommand[0])) {
+			order.setDrink(Order.DrinkType.CHOCOLATE);
+		} else if ("C".equals(splittedCommand[0])) {
+			order.setDrink(Order.DrinkType.COFFEE);
+		} else if ("O".equals(splittedCommand[0])) {
+			order.setDrink(Order.DrinkType.ORANGE_JUICE);
+		}
 	}
 	
 	
--- src/com/talbo/tdd/coffeeMachineTest/CoffeeMachineTest.java
+++ src/com/talbo/tdd/coffeeMachineTest/CoffeeMachineTest.java
@@ -8,37 +8,93 @@ import com.talbo.tdd.coffeeMachine.CoffeeMachine;
 import com.talbo.tdd.coffeeMachine.Order;
 
 public class CoffeeMachineTest {
-
+	
 	@Test
-	public void teaIstea() {
-		String command = "T:1:0";
+	public void teaIsNotFreeTea() {
+		String command = "T:1:0:0.40";
 		Order order = CoffeeMachine.process(command);
 		assertNotNull(order);
 		assertEquals(order.getDrink(), Order.DrinkType.TEA);
 		assertEquals(order.getNbSugar(), new Integer(1));
 		assertEquals(order.isHasStick(), true);
+		assertEquals(order.getPrice(), new Double(0.40));
+		assertEquals(order.isExtraHot(), false);
 	}
 	
 	@Test
-	public void chocolateIsChocolate() {
-		String command = "H::";
+	public void chocolateIsNotFreeChocolate() {
+		String command = "H:::0.50";
 		Order order = CoffeeMachine.process(command);
 		assertNotNull(order);
 		assertEquals(order.getDrink(), Order.DrinkType.CHOCOLATE);
 		assertEquals(order.getNbSugar(), new Integer(0));
 		assertEquals(order.isHasStick(), false);
-		
+		assertEquals(order.getPrice(), new Double(0.50));
+		assertEquals(order.isExtraHot(), false);
 	}
 	
 	@Test
-	public void coffeeIsCoffee() {
-		String command = "C:2:0";
+	public void coffeeIsNotFreeCoffee() {
+		String command = "C:2:0:0.60";
 		Order order = CoffeeMachine.process(command);
 		assertNotNull(order);
 		assertEquals(order.getDrink(), Order.DrinkType.COFFEE);
 		assertEquals(order.getNbSugar(), new Integer(2));
 		assertEquals(order.isHasStick(), true);
+		assertEquals(order.getPrice(), new Double(0.60));
+		assertEquals(order.isExtraHot(), false);
 		
 	}
+	
+	@Test
+	public void orangeIsOrange() {
+		String command = "O:::0.60";
+		Order order = CoffeeMachine.process(command);
+		assertNotNull(order);
+		assertEquals(order.getDrink(), Order.DrinkType.ORANGE_JUICE);
+		assertEquals(order.getNbSugar(), new Integer(0));
+		assertEquals(order.isHasStick(), false);
+		assertEquals(order.getPrice(), new Double(0.60));
+		assertEquals(order.isExtraHot(), false);
+	}
+	
+	@Test
+	public void hotCoffeeIsHotCoffee() {
+		String command = "Ch:::0.60";
+		Order order = CoffeeMachine.process(command);
+		assertNotNull(order);
+		assertEquals(order.getDrink(), Order.DrinkType.COFFEE);
+		assertEquals(order.getNbSugar(), new Integer(0));
+		assertEquals(order.isHasStick(), false);
+		assertEquals(order.getPrice(), new Double(0.60));
+		assertEquals(order.isExtraHot(), true);
+		
+	}
+	
+	@Test
+	public void hotChocolateIsHotChocolate() {
+		String command = "Hh:1:0:0.50";
+		Order order = CoffeeMachine.process(command);
+		assertNotNull(order);
+		assertEquals(order.getDrink(), Order.DrinkType.CHOCOLATE);
+		assertEquals(order.getNbSugar(), new Integer(1));
+		assertEquals(order.isHasStick(), true);
+		assertEquals(order.getPrice(), new Double(0.50));
+		assertEquals(order.isExtraHot(), true);
+	}
+	
+	@Test
+	public void hotTeaIsHotTea() {
+		String command = "Th:2:0:0.40";
+		Order order = CoffeeMachine.process(command);
+		assertNotNull(order);
+		assertEquals(order.getDrink(), Order.DrinkType.TEA);
+		assertEquals(order.getNbSugar(), new Integer(2));
+		assertEquals(order.isHasStick(), true);
+		assertEquals(order.getPrice(), new Double(0.40));
+		assertEquals(order.isExtraHot(), true);
+	}
+	
+	
 
 }
